37c50711ef9bddd6c38f9d0dca3dcda2edc669fb
AlexandraLukacs <lszandika15@gmail.com>
2024-05-17 10:27:18 +0200

    beszuras

--- Adatbazis.php
+++ Adatbazis.php
@@ -16,11 +16,11 @@ class Adatbazis {
             $this->adatbazis
         );
         $siker = 0;
-        if ($this->kapcsolat->connect_errno){
-            $siker = "Nem sikerült a kapcsolat!";
-        }else{
-            $siker = "Sikerült a kapcsolat!";
+        if ($this->kapcsolat->connect_errno) {
+            $siker = "Nem sikerült a kapcsolat";
         }
+        else
+            $siker = "Sikerült a kapcsolat";
         //echo $siker;
         $this->kapcsolat->query("SET NAMES UTF8");
     }
@@ -29,9 +29,8 @@ class Adatbazis {
         $sql = "SELECT $oszlop FROM $tabla";
         return $this->kapcsolat->query($sql);
     }
-
     public function megjelenit($matrix){
-        while ($sor = $matrix->fetch_row()){
+        while ($sor = $matrix->fetch_row()) {
             echo "<img src=\"forras/$sor[0]\" alt=\"forras/$sor[0]\">";
         }
     }
@@ -40,23 +39,22 @@ class Adatbazis {
         $sql = "SELECT $oszlop1, $oszlop2 FROM $tabla";
         return $this->kapcsolat->query($sql);
     }
-
     public function megjelenit2($matrix){
         echo "<table>";
-        echo "<tr><th>Név</th><th>Kép</th></tr>";
-        while ($sor = $matrix->fetch_row()){
+        echo "<tr><th>NÉV</th><th>KÉP</th></tr>";
+        while ($sor = $matrix->fetch_row()) {
             echo "<tr><td>$sor[0]</td><td><img src=\"forras/$sor[1]\" alt=\"forras/$sor[0]\"></td></tr>";
         }
-        echo "</table>";    
+        echo "</table>";
     }
 
     public function modosit($tabla, $oszlop, $regiErtek, $ujErtek){
-        $sql = "UPDATE $tabla SET $oszlop ='$ujErtek' WHERE $oszlop = '$regiErtek'";
+        $sql = "UPDATE $tabla SET $oszlop='$ujErtek' WHERE $oszlop = '$regiErtek'";
         return $this->kapcsolat->query($sql);
     }
 
     public function torles($tabla, $oszlop, $ertek){
-        $sql = "DELETE FROM $tabla WHERE $oszlop ='$ertek'";
+        $sql = "DELETE FROM $tabla WHERE $oszlop = '$ertek'";
         return $this->kapcsolat->query($sql);
     }
 
@@ -64,12 +62,11 @@ class Adatbazis {
         $sql = "SELECT * FROM $tabla";
         return $this->kapcsolat->query($sql)->num_rows;
     }
-
     public function kartyaFeltolt(){
-        $szinOsszeg = $this->rekordokSzama("szin")+1;
-        $formaOsszeg = $this->rekordokSzama("forma")+1;
+        $szinOsszeg = $this->rekordokSzama("szin") + 1;
+        $formaOsszeg = $this->rekordokSzama("forma") + 1;
         for ($szinIndex = 1; $szinIndex < $szinOsszeg; $szinIndex++) { 
-            for ($formaIndex = 1; $formaIndex < $formaOsszeg; $formaIndex++) { 
+            for ($formaIndex = 1; $formaIndex < $formaOsszeg; $formaIndex++){
                 $sql = "INSERT INTO kartyak(szinAzon, formaAzon) VALUES ('$szinIndex','$formaIndex')";
                 $this->kapcsolat->query($sql);
             }
@@ -77,22 +74,29 @@ class Adatbazis {
     }
 
     public function torlesForma($forma){
-        $sql = "DELETE FROM kartyak 
+        $sql = "DELETE FROM kartyak
         WHERE formaAzon IN
         (SELECT formaAzon FROM forma
         WHERE szoveg = '$forma')";
         return $this->kapcsolat->query($sql);
     }
 
-    public function beszurForma($forma){
-        $szam = $this->rekordokSzama("szin");
-        for ($i=1; $i < $szam; $i++) { 
-            $sql = "INSERT INTO kartyak(szinAzon, formaAzon) VALUES ('$i','$forma')";
-            return $this->kapcsolat->query($sql);
+    public function beszurForma($szam){
+        $meret = $this->rekordokSzama("szin");
+        for ($i=1; $i <= $meret; $i++) { 
+            $sql = "INSERT INTO kartyak(szinAzon, formaAzon) VALUES ('$i', '$szam')";
+            $this->kapcsolat->query($sql);
         }
     }
 
-    public function kapcsolatBezar(){
+    public function formaSzam($forma){
+        $sql = "SELECT formaAzon FROM forma WHERE szoveg = '$forma'";
+        $matrix = $this->kapcsolat->query($sql);
+        $sor = $matrix->fetch_array();
+        return $sor[0];
+    }
+
+    public function kapcsoltBezar(){
         $this->kapcsolat->close();
     }
 }
